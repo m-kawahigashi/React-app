d1abb4ebb35fc068e613ccd67e7e87b165926860
m-kawahigashi <50309689+m-kawahigashi@users.noreply.github.com>
2021-09-12 16:14:37 +0900

    update App.jsx 画面大枠を作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,45 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
-}
+  return (
+    <>
+      {/* TODO入力 */}
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
 
+      {/* 未完了TODO一覧 */}
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>あああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>ううう</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+
+      {/* 完了済TODO一覧 */}
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <div>
+            <li>いいい</li>
+            <button>戻す</button>
+          </div>
+          <div>
+            <li>えええ</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
+};
